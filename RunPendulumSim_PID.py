cca5b4479f4b37975fcb5e2b9f9fe3850bc4cce3
Nour Saeed <nour.saeed95@gmail.com>
2020-02-22 20:15:49 +0100

    added run inverted pendulum simulation with PID

---
+++ RunPendulumSim_PID.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pendulum
+from PID import PID_Control
+
+if __name__ =="__main__":
+
+    P = pendulum.Pendulum()
+    PID = PID_Control(50,0,1) # Some values for Kp, Ki and Kd
+
+    # Initial Condition
+    X  = np.array([ [0],
+                    [0],
+                    [0.4],
+                    [0]]
+                    )
+
+    thetas = [float(X[2])]
+    for i in range(0,100):
+
+        u = PID.update(float(X[2]))
+
+        X = P.run_step(X,u)
+
+        x = float(X[0])
+        theta = float(X[2])
+        thetas.append(theta)
+    
+        P.plot_pendulum(x,theta)
+    
+    plt.figure('Response')
+    plt.plot(thetas)
+    plt.show()
